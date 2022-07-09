9dc9eb1e98d34e885624aad0308ca1b1bf9fb510
reflevius <nikita.l@webspace.by>
2022-07-09 13:32:01 +0300

    feat: sequelize define

--- db.js
+++ db.js
@@ -3,17 +3,33 @@ const {
   DataTypes
 } = require('sequelize');
 
-const sequelize = new Sequelize('single_db', 'root', 'root', {
+const sequelize = new Sequelize('sacred-dev', 'root', 'root', {
   host: 'localhost',
   dialect: 'postgres',
+  port: 5432
 });
+const fieldCaption = {
+  field: "caption",
+  type: DataTypes.STRING
+};
+const users = sequelize.define('user', {
+  caption: fieldCaption
+});
+const userData = {
+  caption: 'Hello, from project'
+}
 
-const auth = async () => {
+const auth = async (sequelizeInstance) => {
   try {
-    await sequelize.authenticate();
+    await sequelizeInstance.authenticate();
     console.log('Connection has been established successfully.');
+    await users.sync();
+    console.log('Sync table users');
+    const user = await users.create(userData);
+    console.log(user);
+    console.log(user.id, user.caption, user.createdAt);
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
 }
-auth();
+auth(sequelize);
